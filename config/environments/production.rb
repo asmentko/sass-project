05fa5fb0a389711cc59e77ca843bfcd6668a7c51
asmentko <asmentko@gmail.com>
2018-02-10 20:15:13 +0000

    Added email with SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,20 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Setup the mailer config
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'sass-project-asmentko.herokuapp.com' }  
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
