ba7c8a6a5cadf4c941f37d11cbcd6305dd6f06d5
asmentko <asmentko@gmail.com>
2018-02-11 01:42:04 +0000

    Complete tenant and users

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,7 +17,8 @@ Devise.setup do |config|
   config.mailer_sender = 'asmentko@mac.com'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  # als uncommented following line
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
@@ -124,7 +125,8 @@ Devise.setup do |config|
   # able to access the website for two days without confirming their account,
   # access will be blocked just in the third day. Default is 0.days, meaning
   # the user cannot access the website without confirming their account.
-  # config.allow_unconfirmed_access_for = 2.days
+  # als uncommented following line and changed to zero days
+  config.allow_unconfirmed_access_for = 0.days
 
   # A period that the user is allowed to confirm their account before their
   # token becomes invalid. For example, if set to 3.days, the user can confirm
@@ -132,7 +134,8 @@ Devise.setup do |config|
   # their account can't be confirmed with the token any more.
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
-  # config.confirm_within = 3.days
+  # als uncommented following line
+  config.confirm_within = 3.days
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
@@ -141,7 +144,8 @@ Devise.setup do |config|
   config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
-  # config.confirmation_keys = [:email]
+  # als uncommented following line
+  config.confirmation_keys = [:email]
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,15 @@ Rails.application.routes.draw do
 
     
   # *MUST* come *BEFORE* devise's definitions (below)
+  # als modified to go to our custom confirmations_controller rather than milia's
   as :user do   
-    match '/user/confirmation' => 'milia/confirmations#update', :via => :put, :as => :update_user_confirmation
+    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
 
+  # als again, confirmations goes our custom confirmations_controller and not milia's
   devise_for :users, :controllers => { 
     :registrations => "milia/registrations",
-    :confirmations => "milia/confirmations",
+    :confirmations => "confirmations",
     :sessions => "milia/sessions", 
     :passwords => "milia/passwords", 
   }
